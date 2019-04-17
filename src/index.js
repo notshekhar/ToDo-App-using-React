e56ef1dd00123e6d56fd399dca4e0d88116a9fc6
Shekhar Tyagi <36845555+notshekhar@users.noreply.github.com>
2019-04-18 03:44:59 +0530

    bla

--- src/index.js
+++ src/index.js
@@ -11,43 +11,103 @@ class ToDo extends React.Component {
     return (
       <div>
         <li>
-          <input type="checkbox" />
+          <input
+            type="checkbox"
+            checked={this.props.todo.checked}
+            onClick={this.props.ontoggle}
+          />
+          <span>{this.props.todo.text}</span> &nbsp;&nbsp;
           <button onClick={this.props.ondelete}>delete</button>
-          <span>
-            {this.props.todo.text}
-            {this.props.todo.id}
-          </span>
         </li>
       </div>
     );
   }
 }
-let id = 0;
+
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      todos: []
+      id:
+        localStorage.getItem("todos") &&
+        JSON.parse(localStorage.getItem("todos")).length > 0
+          ? JSON.parse(localStorage.getItem("todos"))[0].id
+          : 0,
+      todos: localStorage.getItem("todos")
+        ? JSON.parse(localStorage.getItem("todos"))
+        : []
     };
   }
   addtodo() {
     let text = prompt("add todo text");
-    this.setState({ todos: [...this.state.todos, { text: text, id: id++ }] });
+    localStorage.setItem(
+      "todos",
+      JSON.stringify([
+        { text: text, id: this.state.id + 1, checked: false },
+        ...this.state.todos
+      ])
+    );
+    this.setState({
+      todos: [
+        { text: text, id: this.state.id + 1, checked: false },
+        ...this.state.todos
+      ]
+    });
+    this.setState({ id: this.state.id + 1 });
   }
   deletetodo(id) {
+    localStorage.setItem(
+      "todos",
+      JSON.stringify(this.state.todos.filter(todo => todo.id != id))
+    );
     this.setState({ todos: this.state.todos.filter(todo => todo.id != id) });
+  }
+  toogletodo(id) {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id != id) {
+          return todo;
+        } else {
+          return { id: todo.id, text: todo.text, checked: !todo.checked };
+        }
+      })
+    });
+    localStorage.setItem(
+      "todos",
+      JSON.stringify(
+        this.state.todos.map(todo => {
+          if (todo.id != id) {
+            return todo;
+          } else {
+            return { id: todo.id, text: todo.text, checked: !todo.checked };
+          }
+        })
+      )
+    );
   }
   render() {
     return (
       <div className="app">
-        <button className="add" onClick={() => this.addtodo()}>
-          Add
-        </button>
-        <ul>
-          {this.state.todos.map(todo => (
-            <ToDo todo={todo} ondelete={() => this.deletetodo(todo.id)} />
-          ))}
-        </ul>
+        <div className="header">
+          <span className="total">
+            Total ToDos: {this.state.todos.length}, Unchecked ToDos:
+            {this.state.todos.filter(todo => !todo.checked).length}
+          </span>
+          <button className="add" onClick={() => this.addtodo()}>
+            Add
+          </button>
+        </div>
+        <div className="content">
+          <ul>
+            {this.state.todos.map(todo => (
+              <ToDo
+                todo={todo}
+                ontoggle={() => this.toogletodo(todo.id)}
+                ondelete={() => this.deletetodo(todo.id)}
+              />
+            ))}
+          </ul>
+        </div>
       </div>
     );
   }
