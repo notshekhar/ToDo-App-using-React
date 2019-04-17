320e507611ee588069fb13e9ba86c856cfb23fa5
Shekhar Tyagi <36845555+notshekhar@users.noreply.github.com>
2019-04-17 20:22:07 +0530

    bla

--- src/index.js
+++ src/index.js
@@ -1,18 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./styles.css";
 
-let styles = {
-  fontFamily: "sans-serif",
-  textAlign: "center",
-  position: "absolute",
-  top: "0px",
-  left: "0px",
-  background: "red",
-  width: "100%",
-  height: "100%",
-  color: "white"
-};
-class App2 extends React.Component {
+class ToDo extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
@@ -20,17 +10,44 @@ class App2 extends React.Component {
   render() {
     return (
       <div>
-        <h1>{this.props.counter}</h1>
+        <li>
+          <input type="checkbox" />
+          <button onClick={this.props.ondelete}>delete</button>
+          <span>
+            {this.props.todo.text}
+            {this.props.todo.id}
+          </span>
+        </li>
       </div>
     );
   }
 }
+let id = 0;
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: []
+    };
+  }
+  addtodo() {
+    let text = prompt("add todo text");
+    this.setState({ todos: [...this.state.todos, { text: text, id: id++ }] });
+  }
+  deletetodo(id) {
+    this.setState({ todos: this.state.todos.filter(todo => todo.id != id) });
+  }
   render() {
     return (
       <div className="app">
-        <h1>Hello World</h1>
-        <App2 counter="1" />
+        <button className="add" onClick={() => this.addtodo()}>
+          Add
+        </button>
+        <ul>
+          {this.state.todos.map(todo => (
+            <ToDo todo={todo} ondelete={() => this.deletetodo(todo.id)} />
+          ))}
+        </ul>
       </div>
     );
   }
